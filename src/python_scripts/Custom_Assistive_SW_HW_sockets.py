57ecf32baa5dd6a6f76a5e1c39e10776c95bb614
jvdn22 <jvandemo7@alumnes.ub.edu>
2025-10-02 08:57:21 +0200

    codi custom sockets actualitzats

--- src/python_scripts/Custom_Assistive_SW_HW_sockets.py
+++ src/python_scripts/Custom_Assistive_SW_HW_sockets.py
@@ -19,10 +19,9 @@ robot = RDK.Item("UR5e")
 base = RDK.Item("UR5e Base")
 tool = RDK.Item('Hand')
 Init_target = RDK.Item('Init')
-App_shake_target = RDK.Item('App_shake')
-Shake_target = RDK.Item('Shake')
-App_give5_target = RDK.Item('App_give5')
-Give5_target = RDK.Item('Give5')
+Init_hi_target = RDK.Item("Init_hi")
+Hi_left_target = RDK.Item("Hi_left")
+Hi_right_target = RDK.Item("Hi_right")
 
 robot.setPoseFrame(base)
 robot.setPoseTool(tool)
@@ -41,20 +40,23 @@ time_high = 3
 # URScript commands
 set_tcp = "set_tcp(p[0.000000, 0.000000, 0.050000, 0.000000, 0.000000, 0.000000])"
 #movej_init = f"movej([-1.009423, -1.141297, -1.870417, 3.011723, -1.009423, 0.000000],1.20000,0.75000,{timel},0.0000)"
+#movel_app_shake = f"movel([-2.268404, -1.482966, -2.153143, -2.647089, -2.268404, 0.000000],{accel_mss},{speed_ms},{timel},0.000)"
+#movel_shake = f"movel([-2.268404, -1.663850, -2.294637, -2.324691, -2.268404, 0.000000],{accel_mss},{speed_ms},{timel/2},0.000)"
+#movel_app_give5 = f"movel([-2.280779, -1.556743, -2.129529, 5.257071, -1.570796, 2.280779],{accel_mss},{speed_ms},{timel},0.000)"
+#movel_give5 = f"movel([-2.195869, -1.642206, -2.040971, 5.253965, -1.570796, 2.195869],{accel_mss},{speed_ms},{timel/2},0.000)"
+
+#Init
 j1, j2, j3, j4, j5, j6 = list(np.radians(Init_target.Joints()).tolist()[0])
 movej_init = f"movej([{j1},{j2}, {j3}, {j4}, {j5}, {j6}],{accel_mss},{speed_ms},{time_high},{blend_r})"
-X, Y, Z, Roll, Pitch, Yaw = Pose_2_TxyzRxyz(App_shake_target.Pose())
-movel_app_shake = f"movel(p[{X}, {Y}, {Z}, {Roll}, {Pitch}, {Yaw}], a={accel_mss}, v={speed_ms}, t={timel}, r={blend_r})"
-#movel_app_shake = f"movel([-2.268404, -1.482966, -2.153143, -2.647089, -2.268404, 0.000000],{accel_mss},{speed_ms},{timel},0.000)"
-X, Y, Z, Roll, Pitch, Yaw = Pose_2_TxyzRxyz(Shake_target.Pose())
-movel_shake = f"movel(p[{X}, {Y}, {Z}, {Roll}, {Pitch}, {Yaw}], a={accel_mss}, v={speed_ms}, t={timel}, r={blend_r})"
-#movel_shake = f"movel([-2.268404, -1.663850, -2.294637, -2.324691, -2.268404, 0.000000],{accel_mss},{speed_ms},{timel/2},0.000)"
-X, Y, Z, Roll, Pitch, Yaw = Pose_2_TxyzRxyz(App_give5_target.Pose())
-movel_app_give5 = f"movel(p[{X}, {Y}, {Z}, {Roll}, {Pitch}, {Yaw}], a={accel_mss}, v={speed_ms}, t={timel}, r={blend_r})"
-#movel_app_give5 = f"movel([-2.280779, -1.556743, -2.129529, 5.257071, -1.570796, 2.280779],{accel_mss},{speed_ms},{timel},0.000)"
-X, Y, Z, Roll, Pitch, Yaw = Pose_2_TxyzRxyz(Give5_target.Pose())
-movel_give5 = f"movel(p[{X}, {Y}, {Z}, {Roll}, {Pitch}, {Yaw}], a={accel_mss}, v={speed_ms}, t={timel}, r={blend_r})"
-#movel_give5 = f"movel([-2.195869, -1.642206, -2.040971, 5.253965, -1.570796, 2.195869],{accel_mss},{speed_ms},{timel/2},0.000)"
+#Init_hi
+j1, j2, j3, j4, j5, j6 = list(np.radians(Init_hi_target.Joints()).tolist()[0])
+movej_init_hi = f"movej([{j1},{j2}, {j3}, {j4}, {j5}, {j6}],{accel_mss},{speed_ms},{time_high},{blend_r})"
+#Hi_right
+X, Y, Z, Roll, Pitch, Yaw = Pose_2_TxyzRxyz(Hi_right_target.Pose())
+movel_hi_right = f"movel(p[{X}, {Y}, {Z}, {Roll}, {Pitch}, {Yaw}], a={accel_mss}, v={speed_ms}, t={timel}, r={blend_r})"
+#Hi_left
+X, Y, Z, Roll, Pitch, Yaw = Pose_2_TxyzRxyz(Hi_left_target.Pose())
+movel_hi_left = f"movel(p[{X}, {Y}, {Z}, {Roll}, {Pitch}, {Yaw}], a={accel_mss}, v={speed_ms}, t={timel}, r={blend_r})"
 
 # Check robot connection
 def check_robot_port(ip, port):
@@ -88,42 +90,29 @@ def Init():
     else:
         print("UR5e not connected. Simulation only.")
 
-def Hand_shake():
-    print("Hand Shake")
-    robot.setSpeed(20)
-    robot.MoveL(App_shake_target, True)
-    robot.setSpeed(100)
-    robot.MoveL(Shake_target, True)
-    robot.MoveL(App_shake_target, True)
-    print("Hand Shake FINISHED")
+def hi():
+    print("Hi!")
+    robot.setSpeed(50)
+    robot.MoveL(Init_hi_target, True)
+    robot.MoveL(Hi_right_target, True)
+    robot.MoveL(Hi_left_target, True)
+    robot.MoveL(Hi_right_target, True)
+    robot.MoveL(Hi_left_target, True)
+    robot.MoveL(Init_hi_target, True)
+    print("Hi FINISHED")
     if robot_is_connected:
-        print("App_shake REAL UR5e")
+        print("App_hi REAL UR5e")
         send_ur_script(set_tcp)
         receive_response(1)
-        send_ur_script(movel_app_shake)
+        send_ur_script(movej_init_hi)
         receive_response(timel)
-        send_ur_script(movel_shake)
+        send_ur_script(movel_hi_right)
         receive_response(timel)
-        send_ur_script(movel_app_shake)
+        send_ur_script(movel_hi_left)
         receive_response(timel)
-
-def Give_me_5():
-    print("Give me 5!")
-    robot.setSpeed(20)
-    robot.MoveL(App_give5_target, True)
-    robot.setSpeed(100)
-    robot.MoveL(Give5_target, True)
-    robot.MoveL(App_give5_target, True)
-    print("Give me 5! FINISHED")
-    if robot_is_connected:
-        print("Give5 REAL UR5e")
-        send_ur_script(set_tcp)
-        receive_response(1)
-        send_ur_script(movel_app_give5)
+        send_ur_script(movel_hi_right)
         receive_response(timel)
-        send_ur_script(movel_give5)
-        receive_response(timel)
-        send_ur_script(movel_app_give5)
+        send_ur_script(movel_hi_left)
         receive_response(timel)
 
 # Confirmation dialog to close RoboDK
@@ -147,9 +136,7 @@ def confirm_close():
 def main():
     global robot_is_connected
     robot_is_connected = check_robot_port(ROBOT_IP, ROBOT_PORT)
-    Init()
-    Hand_shake()
-    Give_me_5()
+    hi()
     if robot_is_connected:
         robot_socket.close()
 
